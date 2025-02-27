fa6ba23df9c11c48954521cf2a4fd446511009de
DamiánQuezada <damianquezada.s@gmail.com>
2025-02-27 17:39:23 -0500

    Modified Pharmacy theme

--- src/app/services/section-theme.service.ts
+++ src/app/services/section-theme.service.ts
@@ -14,7 +14,7 @@ export class SectionThemeService {
     const themes: Record<string, Theme> = {
       groceries: { image: 'assets/red-panda.jpg', bgColor: 'bg-amber-100/70', btnColor: 'bg-amber-700/80', navColor: 'text-amber-700/70'},
       veggies: { image: 'assets/beaver.jpg', bgColor: 'bg-green-100', btnColor: 'bg-emerald-700/60', navColor: 'text-emerald-800/80'},
-      pharmacy: { image: 'assets/snow-leopard2.jpg', bgColor: 'bg-sky-100', btnColor: 'bg-sky-700/80', navColor: 'text-sky-700/80'}
+      pharmacy: { image: 'assets/snow-leopard3.jpg', bgColor: 'bg-sky-100', btnColor: 'bg-sky-700/80', navColor: 'text-sky-700/80'}
     }
 
     return themes[this.currentSection()] ?? { image: '', bgColor: 'bg-groceries', btnColor: 'btn-groceries'}
